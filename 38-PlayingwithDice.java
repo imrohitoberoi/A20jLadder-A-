09e29030eb096485e8401a4dec871c081c181fd2
Rohit oberoi <52903628+imrohitoberoi@users.noreply.github.com>
2020-05-12 21:57:46 +0530

    Add files via upload

---
+++ 38-PlayingwithDice.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	static BufferedReader reader;
+	static StringTokenizer tokenizer;
+	static PrintWriter writer;
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	static String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+		return tokenizer.nextToken();
+	}
+
+	 static class R implements Comparable<R>{
+	        int x, y;
+	        public  R(int x, int y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	        public int compareTo(R o) {
+            		return x-o.x;     //Increasing order(Which is usually required)
+        		}
+	    }   
+
+	public static void main(String[] args) throws IOException {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		tokenizer = null;
+		writer = new PrintWriter(System.out);
+		solve();
+		reader.close();
+		writer.close();
+	}
+	
+	private static void solve() throws IOException {
+		    int a = nextInt();
+	   	    int b = nextInt();
+	    	int x = 0, y = 0, z = 0;
+            for (int i = 1; i <= 6; ++i) {
+            if (Math.abs(a - i) == Math.abs(b - i)) ++y;
+            if (Math.abs(a - i) < Math.abs(b - i)) ++x;
+            if (Math.abs(a - i) > Math.abs(b - i)) ++z;
+  }
+		writer.println(x+" "+y+" "+z);
+	}
+}
