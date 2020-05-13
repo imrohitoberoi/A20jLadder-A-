ae1bbc0d18179f0923cbd4ab16fcc6b16e02f158
Rohit oberoi <52903628+imrohitoberoi@users.noreply.github.com>
2020-05-13 07:35:54 +0530

    Add files via upload

---
+++ 40-Greg'sWorkout.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	static BufferedReader reader;
+	static StringTokenizer tokenizer;
+	static PrintWriter writer;
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	static String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+		return tokenizer.nextToken();
+	}
+
+	 static class R implements Comparable<R>{
+	        int x, y;
+	        public  R(int x, int y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	        public int compareTo(R o) {
+            		return x-o.x;     //Increasing order(Which is usually required)
+        		}
+	    }   
+
+	public static void main(String[] args) throws IOException {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		tokenizer = null;
+		writer = new PrintWriter(System.out);
+		solve();
+		reader.close();
+		writer.close();
+	}
+	
+	private static void solve() throws IOException {
+		int n = nextInt();
+		int[] a=new int[n];
+		int[] s=new int[3];
+		for(int i=0;i<n;i++){
+			a[i]=nextInt();
+			s[i%3]+=a[i];
+		}
+		if(s[0]>=s[1]&&s[0]>=s[2])
+   			        writer.println("chest");
+     		   else if(s[1]>=s[0]&&s[1]>=s[2])
+        			   writer.println("biceps");
+       		 else
+         			  writer.println("back");
+		
+		
+		
+	}
+}
