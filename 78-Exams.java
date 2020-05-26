241e8792c06cf645c73ebc8ce763d3da5ee1b8c1
Rohit oberoi <52903628+imrohitoberoi@users.noreply.github.com>
2020-05-26 09:34:24 +0530

    Add files via upload

---
+++ 78-Exams.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	static BufferedReader reader;
+	static StringTokenizer tokenizer;
+	static PrintWriter writer;
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	static String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+		return tokenizer.nextToken();
+	}
+	//String str=nextToken();
+	//String[] s = str.split("\\s+");
+	public static int gcd(int x, int y){
+		if (y == 0) return x; else return gcd(y, x % y);
+	}
+	 static class R implements Comparable<R>{
+	        int x, y;
+	        public  R(int x, int y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	        public int compareTo(R o) {
+            		return x-o.x;     //Increasing order(Which is usually required)
+        		}
+	    }   
+
+	public static void main(String[] args) throws IOException {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		tokenizer = null;
+		writer = new PrintWriter(System.out);
+		solve();
+		reader.close();
+		writer.close();
+	}
+	
+	private static void solve() throws IOException {
+		int n = nextInt();
+		int k = nextInt();
+		int c=2*n;
+		int d=k-c;
+		writer.println(d>=n?0:n-d);
+	}
+}
