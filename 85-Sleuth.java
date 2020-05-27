bde1dba1966639a85c48e74e7763cd44a0e3f9cb
Rohit oberoi <52903628+imrohitoberoi@users.noreply.github.com>
2020-05-28 05:20:50 +0530

    Add files via upload

---
+++ 85-Sleuth.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	static BufferedReader reader;
+	static StringTokenizer tokenizer;
+	static PrintWriter writer;
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	static String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+		return tokenizer.nextToken();
+	}
+	static String nexts() throws IOException {
+	    tokenizer = new StringTokenizer(reader.readLine());
+	    String s="";
+	    while (tokenizer.hasMoreTokens()) {
+			s+=tokenizer.nextElement()+" ";
+		}
+		return s;
+	}
+	//String str=nextToken();
+	//String[] s = str.split("\\s+");
+	public static int gcd(int x, int y){
+		if (y == 0) return x; else return gcd(y, x % y);
+	}
+	 static class R implements Comparable<R>{
+	        int x, y;
+	        public  R(int x, int y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	        public int compareTo(R o) {
+            		return x-o.x;     //Increasing order(Which is usually required)
+        		}
+	    }   
+
+	public static void main(String[] args) throws IOException {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		tokenizer = null;
+		writer = new PrintWriter(System.out);
+		solve();
+		reader.close();
+		writer.close();
+	}
+	
+	private static void solve() throws IOException {
+		String s = nexts();
+        int l = s.length();
+        int i = l-1;
+        //writer.println(i);
+        while(s.charAt(i) == ' ' || s.charAt(i) == '?'){
+            i--;
+        }
+        //writer.println(i);
+        if (s.charAt(i) == 'a' ||s.charAt(i) == 'A' ||s.charAt(i) == 'e' ||s.charAt(i) == 'E' ||s.charAt(i) == 'i' ||s.charAt(i) == 'I' ||s.charAt(i) == 'o' ||s.charAt(i) == 'O' ||s.charAt(i) == 'y' ||s.charAt(i) == 'Y' ||s.charAt(i) == 'u' ||s.charAt(i) == 'U'){
+            writer.println("YES");    
+        }
+        else{
+            writer.println("NO");
+        }
+    }
+}
