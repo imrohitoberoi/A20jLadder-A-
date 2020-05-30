212e10f19cc11f89074012dd78cc7df582f23184
Rohit oberoi <52903628+imrohitoberoi@users.noreply.github.com>
2020-05-30 20:33:43 +0530

    Add files via upload

---
+++ 90-WormsEvolution.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+	static BufferedReader reader;
+	static StringTokenizer tokenizer;
+	static PrintWriter writer;
+
+	static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	static double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	static String nextToken() throws IOException {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			tokenizer = new StringTokenizer(reader.readLine());
+		}
+		return tokenizer.nextToken();
+	}
+	static String nexts() throws IOException {
+	  	  tokenizer = new StringTokenizer(reader.readLine());
+	   	 String s="";
+	   	 while (tokenizer.hasMoreTokens()) {
+			s+=tokenizer.nextElement()+" ";
+		}
+		return s;
+	}
+	//String str=nextToken();
+	//String[] s = str.split("\\s+");
+	public static int gcd(int x, int y){
+		if (y == 0) return x; else return gcd(y, x % y);
+	}
+	 static class R implements Comparable<R>{
+	        int x, y;
+	        public  R(int x, int y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	        public int compareTo(R o) {
+            		return x-o.x;     //Increasing order(Which is usually required)
+        		}
+	    }   
+
+	public static void main(String[] args) throws IOException {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		tokenizer = null;
+		writer = new PrintWriter(System.out);
+		solve();
+		reader.close();
+		writer.close();
+	}
+	
+	private static void solve() throws IOException {
+	    int n = nextInt();
+		int[] a = new int[100];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				for (int k = j+1; k < n; k++) {
+					if (a[i] + a[j] == a[k]) {
+						writer.print((k + 1) + " " + (i + 1) + " " + (j + 1));
+						return;
+					}
+					if (a[j] + a[k] == a[i]) {
+						writer.print((i + 1) + " " + (j + 1) + " " + (k + 1));
+						return;
+					}
+					if (a[i] + a[k] == a[j]) {
+						writer.print((j + 1) + " " + (i + 1) + " " + (k + 1));
+						return;
+					}
+				}
+			}
+		}
+		writer.print(-1);
+	}
+}
